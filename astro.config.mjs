762fac25521980d2f158b66eeb489733630fb0da
tsprongs301 <torisawa.halu@gmail.com>
2026-05-12 14:16:17 +0900

    Switch to github.io domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // あなたのGitHubユーザー名に合わせたアドレス
+  site: 'https://tsprings301.github.io',
+  // リポジトリ名
+  base: '/portfolio',
+});
